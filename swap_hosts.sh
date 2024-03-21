c70fd7d59a1dfc42aa091b80e565a128caf89ce2
Mr-LongFly <45267764+Mr-LongFly@users.noreply.github.com>
2024-03-21 17:58:09 +0800

    Add files via upload

---
+++ swap_hosts.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# 检查是否提供了输入文件
+if [ -z "$1" ]; then
+    echo "Usage: $0 <input_file>"
+    exit 1
+fi
+
+# 输入文件
+input_file="$1"
+
+# 使用 awk 一次性处理整个文件
+awk '
+BEGIN { OFS = FS = " " }
+/^#/ { print; next }
+{ print $2, $1 }' "$input_file" > "$input_file".tmp && mv "$input_file".tmp "$input_file"
+
+echo "处理完成。"
